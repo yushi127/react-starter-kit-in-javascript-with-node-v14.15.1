9f1d8762dab13a57205a3186348bbdfd476aa62b
福山祐史 <fukuyamayuuji@fukuyamayuujinoMacBook-Air.local>
2024-06-11 23:54:52 +0900

    permit audio and video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function App() {
-  return <div>Hello, React!</div>;
+const getMedia = async () => {
+  const constraints={audio:true,video: true};
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch (err) {
+    /* エラーを処理 */
+  }
+}
+
+getMedia();
+
+const App = () => {
+  return <div>Hello, Reacta!</div>;
 }
 
 export default App;
